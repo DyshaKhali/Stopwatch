ec02abc74e14e5527590328d2c6f67513f0178cf
DYSHA <71291957+DyshaKhali@users.noreply.github.com>
2023-02-10 00:27:44 +0500

    Add files via upload

---
+++ Stopwatch/Stopwatch/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.StartBut = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StopBut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartBut
+            // 
+            this.StartBut.Location = new System.Drawing.Point(12, 142);
+            this.StartBut.Name = "StartBut";
+            this.StartBut.Size = new System.Drawing.Size(156, 47);
+            this.StartBut.TabIndex = 0;
+            this.StartBut.Text = "START";
+            this.StartBut.UseVisualStyleBackColor = true;
+            this.StartBut.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StopBut
+            // 
+            this.StopBut.Location = new System.Drawing.Point(235, 142);
+            this.StopBut.Name = "StopBut";
+            this.StopBut.Size = new System.Drawing.Size(156, 47);
+            this.StopBut.TabIndex = 1;
+            this.StopBut.Text = "STOP";
+            this.StopBut.UseVisualStyleBackColor = true;
+            this.StopBut.Click += new System.EventHandler(this.StopBut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(407, 196);
+            this.Controls.Add(this.StopBut);
+            this.Controls.Add(this.StartBut);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartBut;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button StopBut;
+    }
+}
+
---
+++ Stopwatch/Stopwatch/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        private Font FntTW = new Font("Arial", 84);
+        int ValueStopwatch = 0;
+        int Calc = 0;
+        bool Flash = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.ResizeRedraw | ControlStyles.UserPaint, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void Start()
+        {
+            timer1.Enabled = true;
+            StopBut.Text = "STOP";
+            Invalidate();
+        }
+
+        void Stop()
+        {
+            if (timer1.Enabled)
+            {
+                StopBut.Text = "RESET";
+            }
+            else
+            {
+                ValueStopwatch = 0;
+                StopBut.Text = "STOP";
+            }
+
+            timer1.Enabled = false;
+
+            Invalidate();
+        }
+
+        //
+        void Tick()
+        {
+            Flash = Flash ? false : true;
+
+            if (Calc == 2) { ValueStopwatch++; }
+            Calc = Calc >= 2 ? 0 : Calc += 1;
+
+            if (ValueStopwatch == 60)
+            {
+                MessageBox.Show("One minute!");
+            }
+
+            Invalidate();
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Tick();
+        }
+
+        private void StopBut_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+            string s = Flash ? "{0:00}   {1:00}" : "{0:00} : {1:00}";
+
+            e.Graphics.DrawString(string.Format(s, ValueStopwatch / 60, ValueStopwatch % 60), FntTW, Brushes.WhiteSmoke, new PointF());
+        }
+    }
+}
